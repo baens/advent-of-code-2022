fa4590667c143061423109889ddbe423ecc0fb38
Erik Lindblom <elindblom@overstock.com>
2022-12-04 07:15:19 -0700

    Day 4 done

---
+++ day-4/part-2.main.kts
@@ -0,0 +1,22 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+
+val result = File("./input")
+  .readLines()
+  .map {
+    val rawRanges = it.trim().split(",")
+    val range1 = rawRanges[0].toRangeSet()
+    val range2 = rawRanges[1].toRangeSet()
+    val intersection = range1.intersect(range2)
+    intersection.isNotEmpty()
+  }.count { it }
+
+println(result)
+
+fun String.toRangeSet(): Set<Int> {
+  val split = this.split("-")
+  val lowerRange = split[0].toInt()
+  val upperRange = split[1].toInt()
+  return lowerRange.rangeTo(upperRange).toSet()
+}
