c0b39081b54f210d11a6d68f34325239f392839f
Erik Lindblom <elindblom@overstock.com>
2022-12-04 07:51:27 -0700

    Add javascript for day 2

---
+++ day-4/part-2.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const result = fs.readFileSync("./input", "utf-8")
+    .split(/\r?\n/)
+    .map(line => {
+        if(line.trim() == "") {
+            return false;
+        }
+        
+        [raw1, raw2] = line.trim().split(",");
+        
+        const range1 = toRange(raw1);
+        const range2 = toRange(raw2);
+
+        const intersection = range1.filter(a=>range2.includes(a));
+        const isOverlapped = intersection.length > 0;
+        
+        return isOverlapped;
+    })
+    .filter(result=>result)
+
+console.log(result.length);
+
+function toRange(raw) {
+    [lower, upper] = raw.split("-").map(item=>parseInt(item))
+    return [...Array(upper-lower+1).keys()].map(i=>i+lower)
+}
